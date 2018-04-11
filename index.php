5cf83d17370e484b366baaeca0a20d7bd1850698
Razvan Raducanu <rrazvan.rraducanu@gmail.com>
2018-04-11 13:18:20 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <a href="http://localhost/CSRF1/change_pass.php?password_new=Popescu&password_conf=Popescu&Change=Change"><img src="images/beach.jpg" width="300px" height="300px"/></a>
+    </body>
+</html>
